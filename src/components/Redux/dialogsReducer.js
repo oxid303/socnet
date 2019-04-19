881b4436a2a7848653597471e959c4c07e81d5c8
Ann <oxid303@gmail.com>
2019-04-19 03:02:44 +0300

    switch case into reducer

--- src/components/Redux/dialogsReducer.js
+++ src/components/Redux/dialogsReducer.js
@@ -1,23 +1,29 @@
 const typeMessage = 'type-message';
 const addMessage = 'add-message';
 
-export const typeMessageCreator = (message) => ({type: typeMessage, message: message});
-export const addMessageCreator = () => ({type: addMessage});
+export const typeMessageCreator = (message) => ({ type: typeMessage, message: message });
+export const addMessageCreator = () => ({ type: addMessage });
 
 const dialogsReducer = (state, action) => {
-  if (action.type === typeMessage) {
-    state.messageTextArea = action.message;
-  };
 
-  if (action.type === addMessage) {
-    if (state.messageTextArea === '') return;
+  switch (action.type) {
+    case typeMessage:
+      state.messageTextArea = action.message;
+      break;
 
-    let newMessage = {
-      id: state.messages.length + 1,
-      message: state.messageTextArea
-    }
-    state.messages.push(newMessage);
-    state.messageTextArea = '';
+    case addMessage:
+      if (state.messageTextArea === '') return;
+
+      let newMessage = {
+        id: state.messages.length + 1,
+        message: state.messageTextArea
+      }
+      state.messages.push(newMessage);
+      state.messageTextArea = '';
+      break;
+
+    default:
+      break;
   };
 
   return state;
--- src/components/Redux/profileReducer.js
+++ src/components/Redux/profileReducer.js
@@ -1,24 +1,30 @@
 const typePost = 'type-post';
 const addPost = 'add-post';
 
-export const typePostCreator = (message) => ({type: typePost, message: message});
-export const addPostCreator = () => ({type: addPost});
+export const typePostCreator = (message) => ({ type: typePost, message: message });
+export const addPostCreator = () => ({ type: addPost });
 
 const profileReducer = (state, action) => {
-  if (action.type === typePost) {
-    state.postTextArea = action.message;
-  };
 
-  if (action.type === addPost) {
-    if (state.postTextArea === '') return;
+  switch (action.type) {
+    case typePost:
+      state.postTextArea = action.message;
+      break;
 
-    let newPost = {
-      id: state.posts.length + 1,
-      message: state.postTextArea,
-      likesCount: 0
-    };
-    state.posts.push(newPost);
-    state.postTextArea = '';
+    case addPost:
+      if (state.postTextArea === '') return;
+
+      let newPost = {
+        id: state.posts.length + 1,
+        message: state.postTextArea,
+        likesCount: 0
+      };
+      state.posts.push(newPost);
+      state.postTextArea = '';
+      break;
+
+    default:
+      break;
   };
 
   return state;
