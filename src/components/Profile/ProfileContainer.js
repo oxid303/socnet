76301c07f80929bd8638616ee6c00bd78e39fd41
Ann <oxid303@gmail.com>
2019-07-27 05:35:34 +0300

    profile update when auth v1

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -7,6 +7,18 @@ import Preloader from '../common/Preloader/Preloader';
 import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isAuth: this.props.isAuth}
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!this.props.match.params.userId && prevProps.isAuth !== this.state.isAuth) {
+      this.setState({isAuth: this.props.isAuth});
+      this.props.getStatus(this.props.authId);
+      this.props.getUser(this.props.authId);
+    }
+  }
 
   componentDidMount() {
     const id = this.props.match.params.userId || this.props.authId || 2;
@@ -31,7 +43,8 @@ let mapStateToProps = (state) => {
     isFetching: state.profile.isFetching,
     status: state.profile.status,
     updateStatus: state.profile.updateStatus,
-    authId: state.auth.id
+    authId: state.auth.id,
+    isAuth: state.auth.isAuth
   }
 }
 
